819084f8eaae80371e6e3e5c0cc9cd67d691d4b4
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-12-19 16:22:31 +0200

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
-/* export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5037/api',
-  apiUrlForImages: 'http://localhost:5037',
-  appName: 'Manisik Umrah Booking Platform',
-  version: '1.0.0',
-  // Stripe configuration (publishable key for client-side)
-  stripe: {
-    publishableKey: 'pk_test_51SXi6ADI8RwYwJs03fV0jNsfKfnYv3OSAhocTnzCqfWx3PFSHRPRKbPDuvkUplrfFTs4uaeD6NMZRdbkxvqIGR9k00BleztKHR',
-    currency: 'SAR'
-  }
-};
- */
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5037/api',
+//   apiUrlForImages: 'http://localhost:5037',
+//   appName: 'Manisik Umrah Booking Platform',
+//   version: '1.0.0',
+//   // Stripe configuration (publishable key for client-side)
+//   stripe: {
+//     publishableKey: 'pk_test_51SXi6ADI8RwYwJs03fV0jNsfKfnYv3OSAhocTnzCqfWx3PFSHRPRKbPDuvkUplrfFTs4uaeD6NMZRdbkxvqIGR9k00BleztKHR',
+//     currency: 'SAR'
+//   }
+// };
+
 
 export const environment = {
   production: false,
